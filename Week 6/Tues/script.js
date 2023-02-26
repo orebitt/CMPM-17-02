e480afe848e5491363985607d1a9efbd250019d5
Andrew <35861377+orebitt@users.noreply.github.com>
2023-02-25 16:07:46 -0800

    added request starter code

--- Week 6/Tues/script.js
+++ Week 6/Tues/script.js
@@ -3,9 +3,28 @@ const request = require("request");
 
 const app = express();
 
+// const API_KEY = ""
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
   console.log("welcome to the root!");
+  
+  /*
+  var url = ""
+  
+	request(url, (error, response, body)=>{
+		
+		// Printing the error if occurred
+		if(error) console.log(error)
+	   
+		// Printing status code
+		console.log(response.statusCode);
+		 
+		// Printing body
+		console.log(body);
+	});
+  */
+  
 });
 
 app.listen(3000, () => {
